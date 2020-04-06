a72f78eadb99e405e34f8cf986e929ddb887166d
Aleksander Waage <alek.ntnu@gmail.com>
2020-04-06 22:39:06 +0200

    showing installed manifest contents

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,7 +7,10 @@ import Mod from './Mod';
 // TODO: find correct mod directory.
 const MODS_DIR = 'C:/Program Files/Epic Games/OuterWilds/OWML/Mods';
 
-const installedMods = fs.readdirSync(MODS_DIR);
+const installedModFolders = fs.readdirSync(MODS_DIR);
+const modManifestPaths = installedModFolders.map(folder => MODS_DIR + "/" + folder + "/manifest.json");
+const manifestJsons = modManifestPaths.map(path => fs.readFileSync(path, { encoding: 'UTF-8'}));
+const manifests = manifestJsons.map(json => JSON.parse(json));
 
 const App = () => (
   <div>
@@ -24,11 +27,18 @@ const App = () => (
         </tr>
       ))}
     </table>
-    <h2>Instaled Mods:</h2>
+    <h2>Installed Mods:</h2>
     <table>
-      { installedMods.map(name => (
+      <tr>
+        <th>Name</th>
+        <th>Author</th>
+        <th>Version</th>
+      </tr>
+      { manifests.map(manifest => (
         <tr>
-          <td>{name}</td>
+          <td>{manifest.name}</td>
+          <td>{manifest.author}</td>
+          <td>{manifest.version}</td>
         </tr>
       ))}
     </table>
