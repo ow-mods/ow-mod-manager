1895c6c40e12e9712dbd807c9871cd23338859c3
Ricardo Lopes <raicuparta@gmail.com>
2020-04-06 19:23:23 +0200

    List stuff inside mods directory

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,18 +1,37 @@
 import React from 'react';
+import fs from 'fs';
 
 import modList from '../mod-list.json';
 import Mod from './Mod';
 
-console.log('puta');
+// TODO: find correct mod directory.
+const MODS_DIR = 'C:/Program Files/Epic Games/OuterWilds/OWML/Mods';
+
+const installedMods = fs.readdirSync(MODS_DIR);
 
 const App = () => (
   <div>
-    { modList.map(repo => (
-      <Mod
-        key={repo}
-        repo={repo}
-      />
-    ))}
+    <h2>Available Mods:</h2>
+    <table>
+      { modList.map(repo => (
+        <tr>
+          <td>
+            <Mod
+              key={repo}
+              repo={repo}
+            />
+          </td>
+        </tr>
+      ))}
+    </table>
+    <h2>Instaled Mods:</h2>
+    <table>
+      { installedMods.map(name => (
+        <tr>
+          <td>{name}</td>
+        </tr>
+      ))}
+    </table>
   </div>
 );
 
