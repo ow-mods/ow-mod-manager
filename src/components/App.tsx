36d54fec36bc17099ca3abe3a8478bc197edb085
Aleksander Waage <alek.ntnu@gmail.com>
2020-04-09 15:42:50 +0200

    removed test stuff

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,9 @@
 import { hot } from 'react-hot-loader/root';
 import React from 'react';
 import ModList from './ModList';
-import { Test } from './Test';
 
 const App = () => (
   <div>
-    <Test />
     <ModList />
   </div>
 )
