8eb766ff090948cc9d10f0b9f8d6e19f24a79343
AmazingAlek <alek.ntnu@gmail.com>
2020-04-09 22:15:24 +0200

    Alek/find local manifests (#9)
    * finding manifests anywhere

--- src/services/get-local-mods.ts
+++ src/services/get-local-mods.ts
@@ -1,18 +1,19 @@
 import fs from 'fs';
+import glob from 'glob-promise'
 import config from '../config.json';
 
-function getLocalMods(): Mod[] {
-  const modsFolder = `${config.owmlPath}/Mods`;
-  const installedModFolders = fs.readdirSync(modsFolder);
-  const modManifestPaths = installedModFolders.map(folder => `${modsFolder}/${folder}/manifest.json`);
-  const manifestJsons = modManifestPaths.map(path => fs.readFileSync(path, { encoding: 'UTF-8'}));
+async function getLocalMods(): Promise<Mod[]> {
+  
+  const manifestPaths = await glob(`${config.owmlPath}/Mods/**/manifest.json`);
+  const manifestJsons = manifestPaths.map(path => fs.readFileSync(path, { encoding: 'UTF-8'}));
+
   const manifests = manifestJsons.map(json => JSON.parse(json));
 
   const mods: Mod[] = manifests.map(manifest => ({
     name: manifest.name,
     author: manifest.author,
     version: manifest.version,
-  }))
+  }));
 
   return mods;
 }
