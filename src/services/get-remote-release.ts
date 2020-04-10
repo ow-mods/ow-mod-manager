a524eb86d32d6df4fee42fee2b4d33cb3a6c7fdc
AmazingAlek <alek.ntnu@gmail.com>
2020-04-10 22:16:24 +0200

    summing all downloads (#24)
    * summing all downloads

--- src/services/get-remote-release.ts
+++ src/services/get-remote-release.ts
@@ -2,13 +2,15 @@
 import axios from 'axios';
 
 async function getRemoteRelease(repo: string): Promise<Release> {
-  return axios.get(`https://api.github.com/repos/${repo}/releases/latest`)
+  return axios.get(`https://api.github.com/repos/${repo}/releases`)
     .then((response) => {
       const { data } = response;
 
       const release: Release = {
-        downloadUrl: data.assets[0].browser_download_url,
-        downloadCount: data.assets[0].download_count,
+        downloadUrl: data[0].assets[0].browser_download_url,
+        downloadCount: data
+          .map((rel: Rel) => (rel.assets.length > 0 ? rel.assets[0].download_count : 0))
+          .reduce((a: number, b: number) => a + b, 0),
       };
 
       return release;
@@ -16,3 +18,9 @@ async function getRemoteRelease(repo: string): Promise<Release> {
 }
 
 export default getRemoteRelease;
+
+type Rel = {
+  assets: {
+    download_count: number;
+  }[];
+};
