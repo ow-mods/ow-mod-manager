0a293a558b9d8cf6a65d0f703532b057ee1c12f1
Aleksander Waage <alek.ntnu@gmail.com>
2020-04-10 19:53:00 +0200

    all releases this time

--- src/services/get-remote-release.ts
+++ src/services/get-remote-release.ts
@@ -2,15 +2,18 @@
 import axios from 'axios';
 
 async function getRemoteRelease(repo: string): Promise<Release> {
-  return axios.get(`https://api.github.com/repos/${repo}/releases/latest`)
+  return axios.get(`https://api.github.com/repos/${repo}/releases`)
     .then((response) => {
       const { data } = response;
 
       const release: Release = {
-        downloadUrl: data.assets[0].browser_download_url,
-        downloadCount: data.assets
-          .map((asset: { download_count: number }) => asset.download_count)
-          .reduce((a: number, b: number) => a + b),
+        downloadUrl: data[0].assets[0].browser_download_url,
+        downloadCount: data.map((rel: {
+          assets: {
+            download_count: number;
+          }[];
+        }) => (rel.assets.length > 0 ? rel.assets[0].download_count : 0))
+          .reduce((a: number, b: number) => a + b, 0),
       };
 
       return release;
