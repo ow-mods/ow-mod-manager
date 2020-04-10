c24407b85140494676c4f7acfb8f7770880513f7
Aleksander Waage <alek.ntnu@gmail.com>
2020-04-10 22:00:47 +0200

    caching releases

--- src/services/get-remote-release.ts
+++ src/services/get-remote-release.ts
@@ -1,7 +1,21 @@
 
 import axios from 'axios';
+import { Dictionary } from 'lodash';
+
+const timeout = 6000;
+const cachedData: Dictionary<Release> = {};
+let cachedTime: Date;
 
 async function getRemoteRelease(repo: string): Promise<Release> {
+  if (cachedData
+    && Object.keys(cachedData).includes(repo)
+    && cachedTime
+    && (new Date().getTime() - cachedTime.getTime()) < timeout) {
+    console.log('using cache');
+    return cachedData[repo];
+  }
+
+  console.log('NOT using cache');
   return axios.get(`https://api.github.com/repos/${repo}/releases/latest`)
     .then((response) => {
       const { data } = response;
@@ -11,6 +25,8 @@ async function getRemoteRelease(repo: string): Promise<Release> {
         downloadCount: data.assets[0].download_count,
       };
 
+      cachedData[repo] = release;
+      cachedTime = new Date();
       return release;
     });
 }
