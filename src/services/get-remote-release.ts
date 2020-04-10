04f83405b57e9d708c224cc5983f6368df30e7b0
Aleksander Waage <alek.ntnu@gmail.com>
2020-04-10 19:13:21 +0200

    summing all downloads

--- src/services/get-remote-release.ts
+++ src/services/get-remote-release.ts
@@ -8,7 +8,9 @@ async function getRemoteRelease(repo: string): Promise<Release> {
 
       const release: Release = {
         downloadUrl: data.assets[0].browser_download_url,
-        downloadCount: data.assets[0].download_count,
+        downloadCount: data.assets
+          .map((asset: { download_count: number }) => asset.download_count)
+          .reduce((a: number, b: number) => a + b),
       };
 
       return release;
