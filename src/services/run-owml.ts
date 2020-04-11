0209a317d76356f6272ea9ce742255d26bb2da81
Ricardo Lopes <raicuparta@gmail.com>
2020-04-11 18:14:50 +0200

    Fix typing errors

--- src/services/run-owml.ts
+++ src/services/run-owml.ts
@@ -2,12 +2,13 @@ import exec from 'child_process';
 import config from '../config.json';
 
 const EXE_FILE = 'OWML.Launcher.exe';
+const options = {
+  shell: true,
+  cwd: config.owmlPath,
+};
 
 function runOwml() {
-  exec.execFile(EXE_FILE, {
-    shell: true,
-    cwd: config.owmlPath,
-  }, (error) => {
+  exec.execFile(EXE_FILE, options, (error) => {
     if (error) {
       throw error;
     }
