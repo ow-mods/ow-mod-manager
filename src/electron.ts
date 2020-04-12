f3cd2a4c18c38ba5a4b7c389c05e6d058a9cea81
Ricardo Lopes <raicuparta@gmail.com>
2020-04-13 00:03:20 +0200

    Remove squirrel

--- src/electron.ts
+++ src/electron.ts
@@ -1,11 +1,6 @@
 import { app, BrowserWindow } from 'electron';
 declare const MAIN_WINDOW_WEBPACK_ENTRY: string;
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) {
-  app.quit();
-}
-
 const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 1000,
