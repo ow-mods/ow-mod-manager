8fdd48c2e1de7fb1b30b0214ac3ca556df4e864a
Ricardo Lopes <raicuparta@gmail.com>
2020-04-13 02:39:42 +0200

    Darker OWML row

--- src/components/ModList/ModTableRow.tsx
+++ src/components/ModList/ModTableRow.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import TableCell from '@material-ui/core/TableCell';
-import TableRow from '@material-ui/core/TableRow';
-import Chip from '@material-ui/core/Chip';
+import { makeStyles, TableCell, TableRow, Chip } from '@material-ui/core';
 
 import { isOutdated } from '../../services/mod-manager';
 import ModActions from './ModActions';
@@ -11,30 +9,43 @@ type Props = {
   isRequired?: boolean;
 };
 
+const useStyles = makeStyles({
+  root: {
+    opacity: 0.5,
+    background: '#252525',
+  },
+});
+
 const ModTableRow: React.FunctionComponent<Props> = ({
   mod,
   isRequired = false,
-}) => (
-  <TableRow key={mod.uniqueName}>
-    <TableCell>{mod.name}</TableCell>
-    <TableCell>{mod.author}</TableCell>
-    <TableCell>
-      {mod.isLoading && 'Loading...'}
-      {!mod.isLoading && mod.localVersion && <Chip label={mod.localVersion} />}
-    </TableCell>
-    <TableCell>
-      {mod.remoteVersion && (
-        <Chip
-          color={isOutdated(mod) ? 'primary' : 'default'}
-          label={mod.remoteVersion}
-        />
-      )}
-    </TableCell>
-    <TableCell>{mod.downloadCount}</TableCell>
-    <TableCell padding="none">
-      <ModActions mod={mod} isRequired={isRequired} />
-    </TableCell>
-  </TableRow>
-);
+}) => {
+  const classes = useStyles();
+
+  return (
+    <TableRow classes={isRequired ? classes : undefined} key={mod.uniqueName}>
+      <TableCell>{mod.name}</TableCell>
+      <TableCell>{mod.author}</TableCell>
+      <TableCell>
+        {mod.isLoading && 'Loading...'}
+        {!mod.isLoading && mod.localVersion && (
+          <Chip label={mod.localVersion} />
+        )}
+      </TableCell>
+      <TableCell>
+        {mod.remoteVersion && (
+          <Chip
+            color={isOutdated(mod) ? 'primary' : 'default'}
+            label={mod.remoteVersion}
+          />
+        )}
+      </TableCell>
+      <TableCell>{mod.downloadCount}</TableCell>
+      <TableCell padding="none">
+        <ModActions mod={mod} isRequired={isRequired} />
+      </TableCell>
+    </TableRow>
+  );
+};
 
 export default ModTableRow;
