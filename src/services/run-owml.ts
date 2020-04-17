c0a1c964556035ad792b41f858b57670f2489c30
Ricardo Lopes <raicuparta@gmail.com>
2020-04-17 20:41:09 +0200

    Close mod manager on game launch

--- src/services/run-owml.ts
+++ src/services/run-owml.ts
@@ -1,4 +1,6 @@
 import exec from 'child_process';
+import { remote } from 'electron';
+
 import config from '../config.json';
 
 const EXE_FILE = 'OWML.Launcher.exe';
@@ -6,13 +8,27 @@ const options = {
   shell: true,
   cwd: config.owmlPath,
 };
+let quitTimeout: NodeJS.Timeout;
+
+function waitAndQuit() {
+  if (quitTimeout) {
+    return;
+  }
+  quitTimeout = setTimeout(remote.app.quit, 1000);
+}
 
 function runOwml() {
   exec.execFile(EXE_FILE, options, (error) => {
     if (error) {
+      if (quitTimeout) {
+        // If there's an error launching the game,
+        // don't close the mod manager.
+        clearTimeout(quitTimeout);
+      }
       throw error;
     }
   });
+  waitAndQuit();
 }
 
 export default runOwml;
