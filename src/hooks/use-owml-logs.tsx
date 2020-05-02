778f6647a63726171b949929b58196448985f577
Ricardo Lopes <raicuparta@gmail.com>
2020-05-02 23:24:54 +0200

    Remove test logs

--- src/hooks/use-owml-logs.tsx
+++ src/hooks/use-owml-logs.tsx
@@ -53,19 +53,7 @@ function getSimpleLine(text: string, type: LogType = 'log'): LogLine {
 }
 
 export const LogsProvider: React.FunctionComponent = ({ children }) => {
-  const [lines, setLines] = useState<LogLine[]>(() => {
-    const l: LogLine[] = [];
-    for (let i = 0; i < 70; i += 1) {
-      l[i] = {
-        modName: 'TEST',
-        text: 'line ' + i,
-        type: 'log',
-        count: 0,
-        id: i,
-      };
-    }
-    return l;
-  });
+  const [lines, setLines] = useState<LogLine[]>([]);
   const [server, setServer] = useState<net.Server>();
   const [isServerRunning, setIsServerRunning] = useState(false);
 
