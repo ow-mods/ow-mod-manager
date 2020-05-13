2f9c3f641131beb6eb61d4d19043bb42fe39c964
Ricardo Lopes <raicuparta@gmail.com>
2020-05-14 01:23:43 +0200

    Use repo url from database

--- src/services/get-mod-database.ts
+++ src/services/get-mod-database.ts
@@ -6,21 +6,22 @@ type RemoteMod = {
   downloadUrl: string;
   downloadCount: number;
   manifest: Manifest;
+  repo: string;
 };
 
 export async function getModDatabase(): Promise<Mod[]> {
   return axios.get(config.modDatabaseUrl).then(({ data }) =>
-    data.map(({ manifest, downloadCount, downloadUrl }: RemoteMod) => {
+    data.map(({ manifest, downloadCount, downloadUrl, repo }: RemoteMod) => {
       const mod: Mod = {
         name: manifest.name,
         author: manifest.author,
         uniqueName: manifest.uniqueName,
         remoteVersion: manifest.version,
         modPath: `${config.owmlPath}/Mods/${manifest.name}`,
-        downloadUrl: downloadUrl,
-        downloadCount: downloadCount,
-        //repo?: string, // TODO missing repo
         isLoading: false,
+        downloadUrl,
+        downloadCount,
+        repo,
       };
 
       return mod;
