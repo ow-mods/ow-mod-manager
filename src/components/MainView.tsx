992f8a2a23a9a47764e240b1f2ddc5d6364f6702
Ricardo Lopes <raicuparta@gmail.com>
2020-05-22 18:24:46 +0200

    Tab icons

--- src/components/MainView.tsx
+++ src/components/MainView.tsx
@@ -1,5 +1,16 @@
 import React, { useState } from 'react';
-import { Container, Tabs, Tab, CssBaseline } from '@material-ui/core';
+import {
+  Container,
+  Tabs,
+  Tab,
+  CssBaseline,
+  makeStyles,
+} from '@material-ui/core';
+import {
+  Build as BuildIcon,
+  Dvr as DvrIcon,
+  Settings as SettingsIcon,
+} from '@material-ui/icons';
 
 import Mods from './Mods';
 import Settings from './Settings';
@@ -13,16 +24,42 @@ enum AppTab {
   Settings,
 }
 
+const useStyles = makeStyles({
+  root: {
+    minHeight: 0,
+  },
+  wrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  },
+});
+
 const MainView = () => {
+  const styles = useStyles();
   const [tab, setTab] = useState<AppTab>(AppTab.Mods);
 
   return (
     <CssBaseline>
       <TopBar>
         <Tabs value={tab} onChange={(event, index) => setTab(index)}>
-          <Tab label="Mods" value={AppTab.Mods} />
-          <Tab label="Logs" value={AppTab.Logs} />
-          <Tab label="Settings" value={AppTab.Settings} />
+          <Tab
+            classes={styles}
+            icon={<BuildIcon />}
+            label="Mods"
+            value={AppTab.Mods}
+          />
+          <Tab
+            classes={styles}
+            icon={<DvrIcon />}
+            label="Logs"
+            value={AppTab.Logs}
+          />
+          <Tab
+            classes={styles}
+            icon={<SettingsIcon />}
+            label="Settings"
+            value={AppTab.Settings}
+          />
         </Tabs>
       </TopBar>
       <LoadingBar />
