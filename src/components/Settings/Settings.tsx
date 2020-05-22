3ed7b345a8476787fe4e2542bbc0ce5de093bd21
Ricardo Lopes <raicuparta@gmail.com>
2020-05-22 18:01:25 +0200

    Vertical settings

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -2,43 +2,41 @@ import React from 'react';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Switch from '@material-ui/core/Switch';
 import { useSettings } from '../../hooks';
-import { Card, CardContent } from '@material-ui/core';
+import { FormGroup } from '@material-ui/core';
 
 const Settings = () => {
   const settings = useSettings();
   const { setSettings, closeOnPlay, logToSocket } = settings;
 
   return (
-    <Card>
-      <CardContent>
-        <FormControlLabel
-          control={
-            <Switch
-              checked={closeOnPlay}
-              onChange={() =>
-                setSettings({
-                  closeOnPlay: !closeOnPlay,
-                })
-              }
-            />
-          }
-          label="Close Mod Manager on game start"
-        />
-        <FormControlLabel
-          control={
-            <Switch
-              checked={logToSocket}
-              onChange={() =>
-                setSettings({
-                  logToSocket: !logToSocket,
-                })
-              }
-            />
-          }
-          label="Send game logs to Mod Manager"
-        />
-      </CardContent>
-    </Card>
+    <FormGroup>
+      <FormControlLabel
+        control={
+          <Switch
+            checked={closeOnPlay}
+            onChange={() =>
+              setSettings({
+                closeOnPlay: !closeOnPlay,
+              })
+            }
+          />
+        }
+        label="Close Mod Manager on game start"
+      />
+      <FormControlLabel
+        control={
+          <Switch
+            checked={logToSocket}
+            onChange={() =>
+              setSettings({
+                logToSocket: !logToSocket,
+              })
+            }
+          />
+        }
+        label="Send game logs to Mod Manager"
+      />
+    </FormGroup>
   );
 };
 
