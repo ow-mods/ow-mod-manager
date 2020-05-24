878ae20d9c89f948d0d540b2de8cded77cdde8f1
Ricardo Lopes <raicuparta@gmail.com>
2020-05-24 18:30:56 +0200

    Prevent running script in places it shouldn't be run

--- install-update.bat
+++ install-update.bat
@@ -1,10 +1,17 @@
-REM TODO: Prevent this from running in wrong scenarios
-echo Waiting for Mod Manager to close...
-ping -n 2 127.0.0.1 >nul
-echo Replacing outdated files with updated ones...
-copy *.* "../" /y >nul
-echo Running Mod Manager...
-cd ..
-start "" "Outer Wilds Mod Manager.exe"
-echo Exiting updater...
-exit
+@ECHO OFF
+if exist ../install-update.bat (
+  echo Waiting for Mod Manager to close...
+  ping -n 2 127.0.0.1 >nul
+  echo Replacing outdated files with updated ones...
+  robocopy . ./.. /e
+  echo Running Mod Manager...
+  cd ..
+  start "" "Outer Wilds Mod Manager.exe"
+  echo Exiting updater...
+  exit
+) else (
+  echo ERROR: Couldn't find install-update.bat in parent directory.
+  echo This script should only be executed by the Outer Wilds Mod Manager, not manually.
+  pause
+  exit
+)
