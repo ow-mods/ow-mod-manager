d50ba3ea5c50d2ab2522b717010f0b6d634e57f9
Ricardo Lopes <raicuparta@gmail.com>
2020-05-24 21:15:19 +0200

    Update React, use stable version

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,6 @@ import * as ReactDOM from 'react-dom';
 
 import App from './components/App';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <App />,
-);
+ReactDOM.unstable_createRoot(
+  document.getElementById('root') as HTMLElement,
+).render(<App />);
