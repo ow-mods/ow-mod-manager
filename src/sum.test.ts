695add825833561b9ad3f3c520be7765c5f1c98d
Ricardo Lopes <raicuparta@gmail.com>
2020-05-30 03:40:23 +0200

    Add jest

---
+++ src/sum.test.ts
@@ -0,0 +1,6 @@
+let number: number;
+
+test('adds 1 + 2 to equal 3', () => {
+  number = 3;
+  expect(1 + 2).toBe(number);
+});
--- webpack.rules.js
+++ webpack.rules.js
@@ -6,18 +6,7 @@ module.exports = [
       loader: 'babel-loader',
       options: {
         cacheDirectory: true,
-        babelrc: false,
-        presets: [
-          ['@babel/preset-env', { targets: { browsers: 'last 1 version' } }],
-          '@babel/preset-typescript',
-          '@babel/preset-react',
-        ],
-        plugins: [
-          // plugin-proposal-decorators is only needed if you're using experimental decorators in TypeScript
-          ['@babel/plugin-proposal-decorators', { legacy: true }],
-          ['@babel/plugin-proposal-class-properties', { loose: true }],
-          'react-hot-loader/babel',
-        ],
+        babelrc: true,
       },
     },
   },
