c2e083e800145f58b21ee40b63cb9d62c61d136f
Ricardo Lopes <raicuparta@gmail.com>
2020-05-30 23:05:22 +0200

    Cover network errors

--- src/services/tests/get-mod-database.test.ts
+++ src/services/tests/get-mod-database.test.ts
@@ -6,30 +6,30 @@ import { getModDatabase, RemoteModDatabase, ModDatabase } from '..';
 
 jest.mock('node-fetch', () => jest.fn());
 
+const correctModManifest: Manifest = {
+  author: 'author',
+  name: 'name',
+  uniqueName: 'uniqueName',
+  version: 'version',
+};
+
+const correctRemoteModDatabase: RemoteModDatabase = {
+  modManager: {
+    downloadUrl: 'MOD_MANAGER_URL',
+    version: 'MOD_MANAGER_VERSION',
+  },
+  releases: [
+    {
+      downloadUrl: '',
+      downloadCount: 0,
+      manifest: correctModManifest,
+      repo: '',
+    },
+  ],
+};
+
 describe('Get mod database', () => {
   it('retrieves correctly shaped mod database', async () => {
-    const modManifest: Manifest = {
-      author: 'author',
-      name: 'name',
-      uniqueName: 'uniqueName',
-      version: 'version',
-    };
-
-    const remoteModDatabase: RemoteModDatabase = {
-      modManager: {
-        downloadUrl: 'MOD_MANAGER_URL',
-        version: 'MOD_MANAGER_VERSION',
-      },
-      releases: [
-        {
-          downloadUrl: '',
-          downloadCount: 0,
-          manifest: modManifest,
-          repo: '',
-        },
-      ],
-    };
-
     const expectedResponse: ModDatabase = {
       modManager: {
         downloadUrl: 'MOD_MANAGER_URL',
@@ -37,11 +37,11 @@ describe('Get mod database', () => {
       },
       mods: [
         {
-          name: modManifest.name,
-          author: modManifest.author,
-          uniqueName: modManifest.uniqueName,
-          remoteVersion: modManifest.version,
-          modPath: `OWML/Mods/${modManifest.uniqueName}`,
+          name: correctModManifest.name,
+          author: correctModManifest.author,
+          uniqueName: correctModManifest.uniqueName,
+          remoteVersion: correctModManifest.version,
+          modPath: `OWML/Mods/${correctModManifest.uniqueName}`,
           downloadUrl: '',
           downloadCount: 0,
           repo: '',
@@ -51,7 +51,7 @@ describe('Get mod database', () => {
     };
 
     (fetch as jest.MockedFunction<typeof fetch>).mockResolvedValueOnce(
-      new Response(JSON.stringify(remoteModDatabase)),
+      new Response(JSON.stringify(correctRemoteModDatabase)),
     );
 
     const response = await getModDatabase('https://test.test');
@@ -90,5 +90,31 @@ describe('Get mod database', () => {
     expect(response.mods[0].errors).toEqual(
       expect.arrayContaining([expect.stringContaining('version')]),
     );
+  });
+
+  it('throws error if fetch throws', async () => {
+    const error = new Error('test connection error');
+
+    (fetch as jest.MockedFunction<typeof fetch>).mockRejectedValueOnce(error);
+
+    try {
+      await getModDatabase('https://test.test');
+      expect(false).toEqual(true);
+    } catch (error) {
+      expect(true).toEqual(true);
+    }
+  });
+
+  it('throws error if response not ok', async () => {
+    (fetch as jest.MockedFunction<typeof fetch>).mockResolvedValue(
+      new Response(JSON.stringify(correctRemoteModDatabase), { status: 404 }),
+    );
+
+    try {
+      await getModDatabase('https://test.test');
+      fail("getModDatabase didn't throw error on 404");
+    } catch (error) {
+      expect(error.message || error).toContain('404');
+    }
   });
 });
