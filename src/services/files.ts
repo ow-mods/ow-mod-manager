0bfe4a9413328bb33c343c2ccc9e1f092c608140
Ricardo Lopes <raicuparta@gmail.com>
2020-06-01 17:46:24 +0200

    Cleanup

--- src/services/files.ts
+++ src/services/files.ts
@@ -2,7 +2,6 @@ import unzip from 'unzipper';
 import fetch from 'node-fetch';
 import fs from 'fs-extra';
 import path from 'path';
-import { remote } from 'electron';
 
 // Defines which portion of the loading bar is for download progress,
 // and the remaining is for unzipping progress.
