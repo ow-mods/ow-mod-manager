14f250aa64b2ed593147fb0f0535e91eea503f18
Ricardo Lopes <raicuparta@gmail.com>
2020-07-02 20:44:46 +0200

    logs text

---
+++ src/static-text.ts
@@ -0,0 +1,8 @@
+export const logsText = {
+  showingLines: (count: number) => `Showing ${count} of `,
+  entries: (count: number) => `${count} entries`,
+  page: (count: number) => `, page ${count}`,
+  showPrevious: (count: number) => `Show previous ${count}`,
+  showNext: (count: number) => `Show previous ${count}`,
+  clearLogs: 'Clear log entries',
+};
