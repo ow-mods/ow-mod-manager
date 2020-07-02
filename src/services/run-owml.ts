bf4d601c60146026af70191a5d70cacc8fb70f28
Ricardo Lopes <raicuparta@gmail.com>
2020-07-02 19:57:35 +0200

    Dialog on launch game

--- src/services/run-owml.ts
+++ src/services/run-owml.ts
@@ -14,20 +14,53 @@ function waitAndQuit() {
   quitTimeout = setTimeout(remote.app.quit, 1000);
 }
 
-export function runOwml(closeManager: boolean, port?: number, openVR = false) {
+export async function runOwml(
+  closeManager: boolean,
+  port?: number,
+  openVR = false,
+) {
   const params = [];
+  // TODO improve this
+  const paramNames = [];
   if (!closeManager && port !== undefined) {
     params.push(`-consolePort ${port}`);
+    paramNames.push('Send game logs to Mod Manager');
   }
   if (openVR) {
     params.push(`-vrmode openvr`);
+    paramNames.push('Force OpenVR mode');
   }
 
-  // spawn(EXE_FILE, params, {
-  //   shell: true,
-  //   cwd: config.owmlPath,
-  //   detached: true,
-  // });
+  if (params.length > 0) {
+    const warningDetail = `You enabled "${paramNames.join('" and "')}".
+
+If you own the Steam version of the game, Steam might show a warning about custom parameters.
+
+The warning might not be visible until you open a Steam window.
+
+If you have SteamVR running, the warning might only be visible while you're using your VR headset.
+
+If you want Steam to stop bothering you about this, you'll have to disable these options in the Mod Manager settings.`;
+
+    const { response, checkboxChecked } = await remote.dialog.showMessageBox({
+      type: 'warning',
+      title: 'Outer Wilds Mod Manager',
+      message: 'Steam might show a warning before starting the game',
+      detail: warningDetail,
+      checkboxLabel: "Don't show this again",
+      buttons: ['OK', 'Cancel'],
+    });
+
+    if (response === 1) {
+      return;
+    }
+  }
+
+  spawn(EXE_FILE, params, {
+    shell: true,
+    cwd: config.owmlPath,
+    detached: true,
+  });
 
   if (closeManager) {
     waitAndQuit();
