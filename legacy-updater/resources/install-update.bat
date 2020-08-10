1967e059dcfe54008e38a2ee3ff08211eb608ee8
Ricardo Lopes <raicuparta@gmail.com>
2020-08-11 00:33:53 +0200

    Add legacy updater

---
+++ legacy-updater/resources/install-update.bat
@@ -0,0 +1,13 @@
+rem This is only used to create a fake update for people updating from old version of Mod Manager
+
+@ECHO OFF
+
+color 9F
+echo This version of Outer Wilds Mod Manager can no longer be updated using this method.
+echo Please go to outerwildsmods.com and download the latest Mod Manager installer.
+echo --
+
+echo Press any key to open outerwildsmods.com...
+pause > nul
+
+start "" http://www.outerwildsmods.com
