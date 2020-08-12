f0870ee1ffb83dfd3e280906b9485103edbbd404
Ricardo Lopes <Raicuparta@users.noreply.github.com>
2020-08-12 18:42:34 +0200

    Default OWML path in user directory instead of app directory (#97)

--- app/subscriptions/settings-subsription.tsx
+++ app/subscriptions/settings-subsription.tsx
@@ -1,17 +1,25 @@
 import { useRecoilState, useSetRecoilState } from 'recoil';
 import { useEffect } from 'react';
+import { remote } from 'electron';
 
 import config from '../config.json';
 import { settingsState, owmlSettingsState } from '../store';
-import { getOwmlDefaultSettingsPath, getOwmlSettingsPath } from '../services';
+import {
+  getOwmlDefaultSettingsPath,
+  getOwmlSettingsPath,
+  writeSettings,
+  getSettingsPath,
+} from '../services';
 import { useFileWatcher, useSettingsFileWatcher } from '../hooks';
+
+const userDataDirectory = remote.app.getPath('userData');
 
 export const SettingsSubscription: React.FunctionComponent = () => {
   const [settings, setSettings] = useRecoilState(settingsState);
   const setOwmlSettings = useSetRecoilState(owmlSettingsState);
 
   const settingsFile = useSettingsFileWatcher<Settings>(
-    config.settingsPath,
+    getSettingsPath(),
     config.defaultSettings
   );
 
@@ -31,6 +39,13 @@ export const SettingsSubscription: React.FunctionComponent = () => {
 
   useEffect(() => {
     console.log('useEffect: SettingsSubscription setSettings');
+    if (!settingsFile.owmlPath) {
+      writeSettings({
+        ...settingsFile,
+        owmlPath: `${userDataDirectory}\\OWML`,
+      });
+    }
+
     setSettings(settingsFile);
   }, [settingsFile, setSettings]);
 
