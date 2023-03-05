73c0489a0941cd448f97e68e1afa29cbea6d694c
Noah Pilarski <34462599+MegaPiggy@users.noreply.github.com>
2023-03-05 16:40:09 -0500

    Add enable/disable all mods button (#184)
    * Add enable/disable all mods button
    
    * Bump version

--- app/components/Mods/ModTableHead.tsx
+++ app/components/Mods/ModTableHead.tsx
@@ -1,15 +1,56 @@
 import React from 'react';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
+import {
+  TableCell,
+  TableHead,
+  TableRow,
+  Tooltip,
+  IconButton,
+  Box,
+} from '@material-ui/core';
+import {
+  CheckBox as CheckBoxIcon,
+  IndeterminateCheckBox as CheckBoxIndeterminateIcon,
+  CheckBoxOutlineBlank as CheckBoxBlankIcon,
+} from '@material-ui/icons';
+
+import { isInstalled, setEnabled } from '../../services';
 
 import { modsText } from '../../helpers/static-text';
 
 type Props = {
   title: string;
+  mods: Mod[];
 };
 
-const ModTableHead: React.FunctionComponent<Props> = ({ title }) => (
+const getEnableTooltip = (mods: Mod[]) => {
+  if (mods.every((mod) => mod.isEnabled)) {
+    return modsText.actions.disable;
+  }
+  if (mods.every((mod) => isInstalled(mod))) {
+    return modsText.actions.enable;
+  }
+  return '';
+};
+
+const getCheckboxIcon = (mods: Mod[]) => {
+  if (mods.every((mod) => mod.isEnabled)) {
+    return <CheckBoxIcon />;
+  }
+  if (mods.some((mod) => mod.isEnabled)) {
+    return <CheckBoxIndeterminateIcon />;
+  }
+  return <CheckBoxBlankIcon />;
+};
+
+const toggleAllMods = (mods: Mod[]) => {
+  const enabled = !mods.every((mod) => mod.isEnabled);
+  mods.forEach((mod) => {
+    if (mod.isRequired) return;
+    setEnabled(mod, enabled);
+  });
+};
+
+const ModTableHead: React.FunctionComponent<Props> = ({ title, mods }) => (
   <TableHead>
     <TableRow>
       <TableCell>{title}</TableCell>
@@ -17,7 +58,24 @@ const ModTableHead: React.FunctionComponent<Props> = ({ title }) => (
       <TableCell width="110px" align="center">
         {modsText.tableHead.version}
       </TableCell>
-      <TableCell width="180px" />
+      <TableCell width="180px">
+        <Box display="flex" justifyContent="space-between">
+          <Tooltip title={getEnableTooltip(mods)}>
+            <span>
+              <IconButton
+                size="small"
+                disabled={
+                  mods.every((mod) => !isInstalled(mod)) ||
+                  mods.every((mod) => mod.isRequired)
+                }
+                onClick={() => toggleAllMods(mods)}
+              >
+                {getCheckboxIcon(mods)}
+              </IconButton>
+            </span>
+          </Tooltip>
+        </Box>
+      </TableCell>
     </TableRow>
   </TableHead>
 );
